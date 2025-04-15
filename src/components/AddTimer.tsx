76f7e1470e95169e14ce6b93b6b672fe847032d0
Stephen <stephenn649@gmail.com>
2025-04-15 11:57:57 -0500

    Utilizes all dispatch actions for the TimersContextProvider

--- src/components/AddTimer.tsx
+++ src/components/AddTimer.tsx
@@ -1,15 +1,19 @@
 import { useRef } from 'react';
-
+import { useTimersContext } from '../store/timersContext.tsx';
 import Button from './UI/Button.tsx';
 import Form, { FormHandle } from './UI/Form.tsx';
 import Input from './UI/Input.tsx';
 
 export default function AddTimer() {
   const form = useRef<FormHandle>(null);
+  const { addTimer } = useTimersContext();
 
   function handleSaveTimer(data: unknown) {
+    // get the data to dispatch the new timer to the context
     const extractedData = data as { name: string; duration: string };
     console.log(extractedData);
+    // The + symbol converts the string to a number
+    addTimer({ name: extractedData.name, duration: +extractedData.duration });
     form.current?.clear();
   }
 
