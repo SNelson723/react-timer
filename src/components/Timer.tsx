e88e30719c96511afa4d272da295350d167f0e94
Stephen <stephenn649@gmail.com>
2025-04-15 13:36:28 -0500

    Can add timers to the Timers list and the context is working fine

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -4,7 +4,8 @@ import { type Timer as TimerProps } from "../store/timersContext.tsx";
 const Timer = ({ name, duration }: TimerProps) => {
   return (
     <Container as="article">
-      <h2>TODO: TIMER NAME</h2>
+      <h2>{name}</h2>
+      <p>{duration}</p>
     </Container>
   );
 };
