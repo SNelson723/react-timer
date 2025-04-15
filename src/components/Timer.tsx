a74e17c33728f673d2b71b535555fae4cd31b3e5
Stephen <stephenn649@gmail.com>
2025-04-15 14:06:43 -0500

    Gets the timers working

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,55 @@
 import Container from "./UI/Container.tsx";
-import { type Timer as TimerProps } from "../store/timersContext.tsx";
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from "../store/timersContext.tsx";
+import { useState, useEffect, useRef } from "react";
 
 const Timer = ({ name, duration }: TimerProps) => {
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const interval = useRef<number | null>(null);
+  const { isRunning } = useTimersContext();
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  } else {
+    setRemainingTime(duration * 1000);
+  }
+
+  // Update the remaining time every 50ms => useEffect keeps the timer running after the component is mounted
+  // useEffect is a hook that runs after the component is mounted
+  // The empty array as the second argument means that the effect will only run once, when the component is mounted
+  // The setInterval function is used to update the remaining time every 50ms
+  // The setRemainingTime function is used to update the remaining time
+  // The remaining time is updated by subtracting 50ms from the previous remaining time
+  // The setInterval function is cleared when the component is unmounted
+  useEffect(() => {
+    let timer: number;
+    if (isRunning) {
+      // initialise the timer
+      timer = setInterval(() => {
+        setRemainingTime((prev) => prev - 50);
+      }, 50);
+
+      // set the interval ref's current => therefore this doesn't clear until remaining time hits 0
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
+
+    // React.Strictmode runs the effect twice in development mode, so we need to clear the interval when the component is unmounted
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 };
--- src/components/Timers.tsx
+++ src/components/Timers.tsx
@@ -1,7 +1,6 @@
 import { useTimersContext } from "../store/timersContext";
 import Timer from "./Timer";
 
-
 const Timers = () => {
   const { timers } = useTimersContext();
   return <ul>
